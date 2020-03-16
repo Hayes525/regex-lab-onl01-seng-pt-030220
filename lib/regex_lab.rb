7c3943a807c5e1a6205ce283c30dcbabe4f253fa
Jordan Hayes <nosafoundation@gmail.com>
2020-03-16 02:33:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -74,22 +74,3 @@ end
 valid_number = "(800)IloveNY"
 
 valid_phone_number?(valid_number)
-def starts_with_a_vowel?(word)
-
-end
-
-def words_starting_with_un_and_ending_with_ing(text)
-
-end
-
-def words_five_letters_long(text)
-
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
-end
